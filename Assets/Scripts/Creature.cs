0820178d94e7edc5bd93c4c93ab9b1f46dd05c16
Skollx <cdmx2530@amerike.edu.mx>
2026-08-31 21:02:46 -0600

    Initial Unity VR project

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCamera;
+
+    public float speed = 1.5f;
+    public float retreatSpeed = 3f;
+
+    void Update()
+    {
+        Vector3 directionToCreature =
+            (transform.position - playerCamera.position).normalized;
+
+        float dot = Vector3.Dot(playerCamera.forward,directionToCreature);
+
+        bool playerIsLooking = dot > 0.7f;
+
+        if (playerIsLooking)
+        {
+            Vector3 directionAway = (transform.position - player.position).normalized;
+
+            transform.position += directionAway * retreatSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position,speed * Time.deltaTime);
+        }
+    }
+}
