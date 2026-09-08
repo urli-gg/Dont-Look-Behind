417ddb4ba93f2d829b9cb908603f12ad21fbac8b
LA MAMI ASKANNNNNNN <cdmx2947@amerike.edu.mx>
2026-09-07 21:02:05 -0600

    Asset de pasillo
    agregamos asset del pasillo

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -9,45 +9,70 @@ public class Creature : MonoBehaviour
     public float speed = 1.5f;
     public float retreatSpeed = 3f;
     public float gameOverDistance = 1.2f;
+    public float lookThreshold = 0.7f;
 
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(
-            transform.position,
-            player.position
+        
+        Vector3 directionToCreature = transform.position - playerCamera.position;
+
+        
+        directionToCreature.y = 0;
+
+        directionToCreature.Normalize();
+
+        
+        Vector3 cameraForward = playerCamera.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+
+        float dot = Vector3.Dot(
+       cameraForward,
+            directionToCreature
         );
 
+        bool playerIsLooking = dot > lookThreshold;
+
+        
+        if (playerIsLooking)
+        {
+            Vector3 directionAway =
+                transform.position - player.position;
+
+            directionAway.y = 0;
+            directionAway.Normalize();
+
+            transform.position +=
+                directionAway * retreatSpeed * Time.deltaTime;
+
+            return;
+        }
+
+        
+        Vector3 creaturePosition = transform.position;
+        Vector3 playerPosition = player.position;
+
+        creaturePosition.y = 0;
+        playerPosition.y = 0;
+
+        float distanceToPlayer =
+            Vector3.Distance(creaturePosition, playerPosition);
+
+        
         if (distanceToPlayer <= gameOverDistance)
         {
             SceneManager.LoadScene("GameOver");
             return;
         }
 
-        Vector3 directionToCreature =
-            (transform.position - playerCamera.position).normalized;
+        
+        Vector3 targetPosition = player.position;
+        targetPosition.y = transform.position.y;
 
-        float dot = Vector3.Dot(
-            playerCamera.forward,
-            directionToCreature
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
         );
-
-        bool playerIsLooking = dot > 0.7f;
-
-        if (playerIsLooking)
-        {
-            Vector3 directionAway =
-                (transform.position - player.position).normalized;
-
-            transform.position +=
-                directionAway * retreatSpeed * Time.deltaTime;
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                player.position,
-                speed * Time.deltaTime
-            );
-        }
     }
 }
