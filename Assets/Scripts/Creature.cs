bd766636081852aef693df08821b83dbf2de640c
Gael Saucedo <saucedo.gael1612@gmail.com>
2026-09-24 02:55:35 -0600

    Escenas de main menu y game over completas, script de creature mejorado

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -11,58 +11,101 @@ public class Creature : MonoBehaviour
     public float gameOverDistance = 1.2f;
     public float lookThreshold = 0.7f;
 
+    
+
+    
+    public int maxTurns = 3;
+
+    
+    private int turnsUsed = 0;
+
+    
+    private bool wasLookingLastFrame = false;
+
+    
+
+
     void Update()
     {
-        
-        Vector3 directionToCreature = transform.position - playerCamera.position;
+        Vector3 directionToCreature =
+            transform.position - playerCamera.position;
 
-        
         directionToCreature.y = 0;
 
         directionToCreature.Normalize();
 
-        
+
         Vector3 cameraForward = playerCamera.forward;
+
         cameraForward.y = 0;
+
         cameraForward.Normalize();
 
-        float dot = Vector3.Dot(cameraForward,directionToCreature);
+
+        float dot = Vector3.Dot(cameraForward, directionToCreature
+        );
+
 
         bool playerIsLooking = dot > lookThreshold;
 
+
         
-        if (playerIsLooking)
+        if (playerIsLooking && !wasLookingLastFrame)
+        {
+            turnsUsed++;
+
+            Debug.Log("Giro número: " + turnsUsed);
+        }
+
+        
+        bool canRepelCreature = playerIsLooking && turnsUsed <= maxTurns;
+
+        
+        wasLookingLastFrame = playerIsLooking;
+
+        
+
+
+        if (canRepelCreature)
         {
             Vector3 directionAway = transform.position - player.position;
 
             directionAway.y = 0;
+
             directionAway.Normalize();
+
 
             transform.position += directionAway * retreatSpeed * Time.deltaTime;
 
             return;
         }
 
-        
+
         Vector3 creaturePosition = transform.position;
         Vector3 playerPosition = player.position;
+
 
         creaturePosition.y = 0;
         playerPosition.y = 0;
 
-        float distanceToPlayer = Vector3.Distance(creaturePosition, playerPosition);
 
-        
+        float distanceToPlayer =
+            Vector3.Distance(creaturePosition,playerPosition);
+
+
         if (distanceToPlayer <= gameOverDistance)
         {
             SceneManager.LoadScene("GameOver");
+
             return;
         }
 
-        
+
         Vector3 targetPosition = player.position;
+
         targetPosition.y = transform.position.y;
 
-        transform.position = Vector3.MoveTowards(transform.position,targetPosition,speed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
 }
